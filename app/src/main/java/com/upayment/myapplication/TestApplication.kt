bf967233fdd0f30a4f0030bef8ff15294d83395d
aqeel2khan <147251311+aqeel2khan@users.noreply.github.com>
2024-01-02 22:22:47 +0300

    Add files via upload
    added file

---
+++ app/src/main/java/com/upayment/myapplication/TestApplication.kt
@@ -0,0 +1,16 @@
+package com.upayment.testapp
+
+import android.app.Application
+import android.widget.Toast
+import com.upayment.upaymentsdk.track.UpaymentGateway
+
+class TestApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val tokenNonWhiteLabel :String = "oxxnDz0ES48qyaT96f8VG6YYyFr0krk2akJI7LH5";
+        val tokenWhiteLabel :String = "aS4OMOvxGd1UDsTgxRBEE1af5BqSaOVDa5eMtch2";
+        UpaymentGateway.init(this,"123",tokenNonWhiteLabel,true);
+
+    }
+}
